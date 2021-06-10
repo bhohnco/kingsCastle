c114df57805e9018dd3e9e6f25ff36492b80b4ef
bhohnco <bryanhohn13@gmail.com>
2021-06-10 07:27:43 -0600

    Header start, needs styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = (props) => {
   return (
       <header className="App-header">
-        <h2>{props.text}</h2>
+        <h2>King's Collection</h2>
       </header>
   );
 };
