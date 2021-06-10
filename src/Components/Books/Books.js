75abff14d23854fa6cda0dbd07686a789b67b4af
bhohnco <bryanhohn13@gmail.com>
2021-06-10 07:27:06 -0600

    Data coming over need duplicates removed

--- src/Components/Books/Books.js
+++ src/Components/Books/Books.js
@@ -2,27 +2,47 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 // import {Link} from 'react-router-dom';
 // import {getAllBooks} from '../../utilities/apiCalls';
-// import utils from '../../utilities/utils';
+import utils from '../../utilities/utils';
 
-const allWorksURL = 'http://openlibrary.org/authors/OL2162284A/works.json'
+const allWorksURL = 'https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=RYjnLebvgEGZoU2o3hlX74KAcas5pRG2'
 
 const Books = () => {
   const [books, setBooks] = useState([])
 
     useEffect(() => {
       const fetchBooks = async () => {
-        const res = await axios.get(allWorksURL)
-        console.log(res.data.entries)
+        const bookData = await axios.get(allWorksURL)
+        const cleanBooks = (bookData) {
+          return [...new Map(bookData.map(item => [item[key], item])).values()]
+        // setBooks(cleanBooks.data.results)
+        console.log(cleanBooks.data.results)
       }
       fetchBooks()
     }, [])
 
       return (
-          <div>
+          <>
           <h1>Books</h1>
-          </div>
+            <section className='card-display'>
+              {books.map ((book) => {
+                const {book_title } = book
+                // const bookImage = `http://covers.openlibrary.org/b/id/${book.title}-L.jpg`
+                // console.log(book.description, book.covers)
+
+                return (
+                    <div className='book-card' key ={book}>
+                     {/*<img src = {} alt ={book.title}/>*/}
+                      {book_title}
+                      </div>
+                )
+
+              })}
+            </section>
+
+          </>
   )
 }
+
 
 
 export default Books;
