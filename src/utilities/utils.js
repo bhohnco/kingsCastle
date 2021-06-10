f008117b7fe9bdf9000d113f9288060f71922ce7
bhohnco <bryanhohn13@gmail.com>
2021-06-10 16:21:08 -0600

    Adding photo function

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -1,14 +1,19 @@
+import coverPhotos from '../assests/coverPhotos'
 
 const utils = {
+
   removeDuplicates(objectArray) {
     return objectArray.reduce((finalOutput, currentObj) => {
       const findTitle = finalOutput.find(item => item.book_title === currentObj.book_title);
       if (!findTitle) {
         return finalOutput.concat([currentObj]).sort((a, b) => a.book_tiltle - b.book_title)
       }
-      console.log(finalOutput)
       return finalOutput
     }, [])
+  }
+
+  addCoverPhotos (coverPhotos) {
+
   }
 }
   export default utils;
