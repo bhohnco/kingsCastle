8cec03d04c366a303bad35c2fb78f9d6b1d046a6
bhohnco <bryanhohn13@gmail.com>
2021-06-13 07:47:34 -0600

    Search bar add to my app page, needs testing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect} from "react";
 import Header from '../Header/Header'
+import SearchBar from "../SearchBar /SearchBar";
 import Books from '../Books/Books'
 import FavoriteButton from "../FavoriteButton/FavoriteButton";
 import CompletedBookButton from "../CompletedBookButton/CompletedBookButton";
@@ -8,8 +9,24 @@ import './_app.scss';
 
 function App() {
 
+  const [searchedBooks, setSearchedBooks] = useState([])
   const [favorites, setFavorites] = useState([])
   const [completedBooks, setCompletedBooks] = useState([])
+
+  const getBookRequest = async (searchValue) => {
+    const url = `http://openlibrary.org/search.json?title=${searchValue}`;
+    const response = await fetch(url);
+    console.log(response)
+    const responseJson = await response.json();
+
+    if (responseJson.Search) {
+      setSearchedBooks(responseJson.Search);
+    }
+  };
+
+  useEffect(() => {
+    getBookRequest(searchedBooks);
+  }, [searchedBooks]);
 
 
 console.log(favorites)
@@ -18,6 +35,9 @@ console.log(favorites)
     <div className="App">
       <div className='site-container'>
       <Header/>
+        <>
+        <SearchBar searchValue={searchedBooks} setSearchValue={setSearchedBooks}/>
+        </>
         <div className='card-display'>
         <h1 className='container-title'>Books</h1>
         <Books
