a95cc084ec0aa876dc33193e456d434cc6afae08
bhohnco <bryanhohn13@gmail.com>
2021-06-13 20:36:28 -0600

    Adjust book state to flow through app

--- src/Components/Books/Books.js
+++ src/Components/Books/Books.js
@@ -1,54 +1,86 @@
 import React, {useEffect, useState} from 'react';
 // import {Link} from 'react-router-dom';
-import {getBooks} from '../../utilities/ApiCalls';
-import utils from '../../utilities/utils';
+import PropTypes from "prop-types";
 
 
-const Books = (props) => {
-console.log(props)
-  const [books, setBooks,] = useState([])
+const Books = ({books, favoriteButtonComponent, favoritesBox, favoritedBooks, searchedBooks }) => {
 
-  const FavoriteComponent = props.favoriteButtonComponent
+  const FavoriteComponent = favoriteButtonComponent
   // const CompletedComponent = props.addCompletedBook
 
+  if (searchedBooks.length) {
+    const searchBooks = books?.filter(book => {
+      return book.title.toLowerCase().includes(searchedBooks.toLowerCase())
+    })
+    return searchBooks.map(book => {
+      const {title, key, covers} = book
+      // let first = function(covers) { return !!covers }
+      // console.log(covers)
+      // let findCoverID = covers.find(first)
+      // console.log(findCoverID)
 
-  useEffect(() => {
-    const fetchBooks = async () => {
-      const bookData = await getBooks()
-      const narrowObject = bookData.entries
-      const finalData= utils.removeDuplicates(narrowObject)
-      setBooks(finalData)
-    }
-    fetchBooks()
-  }, [])
-
-
-
-
-  return books.map (book => {
-    const {title, key, covers} = book
-    // let first = function(covers) { return !!covers }
-    // let findCoverID = covers.find(first)
-    // console.log(findCoverID)
-    const bookImage = `http://covers.openlibrary.org/b/id/${covers}-M.jpg`
-        return <div className="book-link" key={key}>
-          <img className="book-card" style={{
-            backgroundImage: `url(${bookImage})`}}
-               key={key}
-               src={bookImage}
-               alt={title}
-               id={title}
-               />
-          <div className="overlay"/>
-          <FavoriteComponent book={book} favorites={props.favoriteBooksContainer} favoritedBooks={props.favoritedBooks}/>
-          <h3 className="card-title">{title}</h3>
-          <div>
-          </div>
-
+      const bookImage = `http://covers.openlibrary.org/b/id/${covers}-M.jpg`
+      return <div className="book-link" key={key}>
+        <img className="book-card" style={{
+          backgroundImage: `url(${bookImage})`
+        }}
+             key={key}
+             src={bookImage}
+             alt={title}
+             id={title}
+        />
+        <FavoriteComponent book={book} favorites={favoritesBox} favoritedBooks={favoritedBooks}
+                           className='overlay d-flex align-items-center justify-content-center'/>
+        <h3 className="card-title">{title}</h3>
+        <div>
         </div>
-  })
-}
+
+      </div>
+    })
+  }
+
+
+  if (books.length) {
+    return books.map(book => {
+      const {title, key, covers} = book
+      // let first = function(covers) { return !!covers }
+      // console.log(covers)
+      // let findCoverID = covers.find(first)
+      // console.log(findCoverID)
+
+      const bookImage = `http://covers.openlibrary.org/b/id/${covers}-M.jpg`
+      return <div className="book-link" key={key}>
+        <img className="book-card" style={{
+          backgroundImage: `url(${bookImage})`
+        }}
+             key={key}
+             src={bookImage}
+             alt={title}
+             id={title}
+        />
+        <FavoriteComponent book={book} favorites={favoritesBox} favoritedBooks={favoritedBooks}
+                           className='overlay d-flex align-items-center justify-content-center'/>
+        <h3 className="card-title">{title}</h3>
+        <div>
+        </div>
+
+      </div>
+    })
+  } else {
+    return null;
+  }
+};
 
 export default Books;
 
-{/*<CompletedComponent book={book} completedBooks={props.completedList} addCompletedBook={props.completedBooks}/>*/}
+Books.propTypes = {
+  favoritesBox:PropTypes.array,
+  book:PropTypes.array,
+  favoritedBooks:PropTypes.func,
+  favoriteButtonComponent:PropTypes.func,
+  removeFavoriteComponent:PropTypes.func,
+  key: PropTypes.string,
+  title: PropTypes.string,
+  handleClick: PropTypes.string,
+  bookImage: PropTypes.string
+};
