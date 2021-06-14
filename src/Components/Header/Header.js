2e256c54027c81414acbaa8f1f0e381cd268a4fe
bhohnco <bryanhohn13@gmail.com>
2021-06-14 04:22:59 -0600

    Adjusting the butttons in Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,11 @@
 import React from "react";
-// import { Link } from 'react-router-dom'
 
 const Header = ({ handleChange, input, books}) => {
   return (
       <header className="App-header">
-      <h5 className='header-button'>Kings Castle</h5>
+      <h5 className='title'>Kings Castle</h5>
         <nav className='nav-buttons' >
-            <h5 className='home header-button'>Home</h5>
+            <h5 className='home header-button'>The place to find all of Stephen Kings works</h5>
         </nav>
       </header>
   );
