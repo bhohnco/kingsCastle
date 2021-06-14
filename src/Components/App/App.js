ac8ace335ea408902b4d54e710705803544a8ff1
bhohnco <bryanhohn13@gmail.com>
2021-06-13 20:33:24 -0600

    Adjusted book state to flow down from app to book

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,51 +3,52 @@ import Header from '../Header/Header'
 import SearchBar from "../SearchBar /SearchBar";
 import Books from '../Books/Books'
 import FavoriteButton from "../FavoriteButton/FavoriteButton";
-import CompletedBookButton from "../CompletedBookButton/CompletedBookButton";
+import RemoveFavorites from "../RemoveFavorites/RemoveFavorites";
 import './_app.scss';
+import {getBooks} from "../../utilities/ApiCalls";
+import utils from "../../utilities/utils";
+
 
 
 function App() {
 
-  const [searchedBooks, setSearchedBooks] = useState([])
+  const [books, setBooks,] = useState([])
+  const [searchedBooks, setSearchedBooks] = useState('')
   const [favorites, setFavorites] = useState([])
-  const [completedBooks, setCompletedBooks] = useState([])
-
-  const getBookRequest = async (searchValue) => {
-    const url = `http://openlibrary.org/search.json?title=${searchValue}`;
-    const response = await fetch(url);
-    console.log(response)
-    const responseJson = await response.json();
-
-    if (responseJson.Search) {
-      setSearchedBooks(responseJson.Search);
-    }
-  };
 
   useEffect(() => {
-    getBookRequest(searchedBooks);
-  }, [searchedBooks]);
+      const fetchBooks = async () => {
+        const bookData = await getBooks()
+        const narrowObject = bookData.entries
+        const finalData= utils.removeDuplicates(narrowObject)
+        setBooks(finalData)
+      }
+      fetchBooks()
+    }, [])
 
 
-console.log(favorites)
+  const pushSearchResults = (search) => {
+    setSearchedBooks(search)
+  }
+console.log(searchedBooks)
 
   return (
     <div className="App">
       <div className='site-container'>
       <Header/>
         <>
-        <SearchBar searchValue={searchedBooks} setSearchValue={setSearchedBooks}/>
+        <SearchBar pushSearchResults={pushSearchResults} searchValue={searchedBooks} setSearchValue={setSearchedBooks}/>
         </>
         <div className='card-display'>
         <h1 className='container-title'>Books</h1>
-        <Books
-            favorites ={favorites}
-            favoritedBooks ={setFavorites}
-            favoriteButtonComponent ={FavoriteButton}
-            completedBooks = {completedBooks}
-            addCompletedBook = {setCompletedBooks}
-            completeBookComponent = {CompletedBookButton}
-        />
+          <Books
+              books={books}
+              searchedBooks={searchedBooks}
+              bookGroup={setBooks}
+              favoritesBox={favorites}
+              favoritedBooks={setFavorites}
+              favoriteButtonComponent={FavoriteButton}
+              removeFavoriteComponent={RemoveFavorites}/>
         </div>
     </div>
       </div>
