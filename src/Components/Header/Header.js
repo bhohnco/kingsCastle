70d694bece2543943354f6eb99ad145e0720e202
bhohnco <bryanhohn13@gmail.com>
2021-06-15 19:54:30 -0600

    Readlist button add

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 const header = () => {
   return (
       <header className="App-header">
       <h5 className='title'>Kings Castle</h5>
-        <nav className='nav-buttons' >
-            <h5 className='home header-button'>The place to find all of Stephen Kings works</h5>
-        </nav>
+          <div className='page-quote'>The place to find all of Stephen Kings works</div>
+          <Link to = "/readinglist" >
+            <button className="reading-list-button">Go to Reading-List</button>
+          </Link>
       </header>
   );
 };
