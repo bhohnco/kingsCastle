659b09b1ab3312db4b4351271274f2c5b574dac9
bhohnco <bryanhohn13@gmail.com>
2021-06-26 04:28:28 -0400

    Adjusting book cards and page layout

---
+++ src/bookData.js
@@ -0,0 +1,233 @@
+import '../src/assets/kingCovers/JFK.jpeg'
+import '../src/assets/kingCovers/Wovles of Calla.jpeg'
+import '../src/assets/kingCovers/nightmares.jpeg'
+import '../src/assets/kingCovers/blackhouse.jpeg'
+import '../src/assets/kingCovers/the stand.jpeg'
+import '../src/assets/kingCovers/FourPastmidnight.jpeg'
+import '../src/assets/kingCovers/NeedfulThings.png'
+import '../src/assets/kingCovers/Geralds Game.png'
+import '../src/assets/kingCovers/Everythings.jpeg'
+import '../src/assets/kingCovers/IT.png'
+import '../src/assets/kingCovers/full dark no stars .jpeg'
+import '../src/assets/kingCovers/bag of bones .jpeg'
+import '../src/assets/kingCovers/Rose Madder.png'
+import '../src/assets/kingCovers/doctorsleep.jpeg'
+import '../src/assets/kingCovers/Joyland.jpeg'
+import '../src/assets/kingCovers/under the dome.jpeg'
+import '../src/assets/kingCovers/revival.jpeg'
+import '../src/assets/kingCovers/lisey story.jpeg'
+import '../src/assets/kingCovers/Just after sunset.jpeg'
+import '../src/assets/kingCovers/Duma key.jpeg'
+import '../src/assets/kingCovers/Mr. Mercedes.jpeg'
+import '../src/assets/kingCovers/Danse Macabre.jpeg'
+import '../src/assets/kingCovers/cujo.png'
+import '../src/assets/kingCovers/Different Seasons.png'
+import '../src/assets/kingCovers/Christine.jpeg'
+import '../src/assets/kingCovers/petsematary.jpeg'
+import '../src/assets/kingCovers/Skeleton Crew.png'
+import '../src/assets/kingCovers/Eyes of the Dragon.png'
+import '../src/assets/kingCovers/Misery.png'
+import '../src/assets/kingCovers/The Tommyknockers.png'
+import '../src/assets/kingCovers/The Dark Half.png'
+import '../src/assets/kingCovers/thewastelands.jpeg'
+import '../src/assets/kingCovers/NeedfulThings.png'
+import '../src/assets/kingCovers/Insomnia.png'
+import '../src/assets/kingCovers/TOmGordon.jpeg'
+import '../src/assets/kingCovers/On writing.jpeg'
+import '../src/assets/kingCovers/dreamcatcher.jpeg'
+import '../src/assets/kingCovers/buick 8.jpeg'
+import '../src/assets/kingCovers/song of suz.jpeg'
+import '../src/assets/kingCovers/Cell.jpeg'
+import '../src/assets/kingCovers/Finders Keepers.jpeg'
+import '../src/assets/kingCovers/end of watch.jpeg'
+import '../src/assets/kingCovers/outsider.jpeg'
+import '../src/assets/kingCovers/elevation.jpeg'
+import '../src/assets/kingCovers/theInstitute.jpeg'
+
+
+const images = [
+
+    {
+      photoTitle: '11/22/63',
+      photoUrl: '../src/assets/kingCovers/JFK.png',
+    },
+    {
+      photoTitle: 'The Dark Tower V: Wolves of the Calla',
+      photoUrl: '../src/assets/kingCovers/Wovles of Calla.jpeg',
+    },
+    {
+      photoTitle: 'Nightmares and Dreamscapes',
+      photoUrl: '../src/assets/kingCovers/nightmares.jpeg',
+    },
+    {
+      photoTitle: 'Black House',
+      photoUrl: '../src/assets/kingCovers/blackhouse.jpeg',
+    },
+    {
+      photoTitle: 'The Stand',
+      photoUrl: '../src/assets/kingCovers/the stand.jpeg',
+    },
+    {
+      photoTitle: 'Four Past Midnight',
+      photoUrl: '../src/assets/kingCovers/FourPastmidnight.jpeg',
+    },
+    {
+      photoTitle: 'Needful Things',
+      photoUrl: '.../src/assets/kingCovers/NeedfulThings.png',
+    },
+    {
+      photoTitle: 'Gerald\'s Game',
+      photoUrl: '../src/assets/kingCovers/Geralds Game.png',
+    },
+    {
+      photoTitle: 'Everything\'s Eventual : 14 Dark Tales',
+      photoUrl: '../src/assets/kingCovers/Everythings.jpeg',
+    },
+    {
+      photoTitle: 'It',
+      photoUrl: '../src/assets/kingCovers/IT.png',
+    },
+    {
+      photoTitle: 'Full Dark, No Stars',
+      photoUrl: '../src/assets/kingCovers/full dark no stars .jpeg',
+    },
+    {
+      photoTitle: 'Bag of Bones',
+      photoUrl: '../src/assets/kingCovers/bag of bones .jpeg',
+    },
+    {
+      photoTitle: 'Rose Madder',
+      photoUrl: '../src/assets/kingCovers/Rose Madder.png',
+    },
+    {
+      photoTitle: 'Doctor Sleep',
+      photoUrl: '../src/assets/kingCovers/doctorsleep.jpeg',
+    }
+//     // {
+//     //   "photoTitle":'Joyland',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Under the Dome',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Revival',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Lisey\'s Story',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Just After Sunset',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Duma Key',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Mr. Mercedes',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Danse Macabre',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Cujo',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Different Seasons',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Pet Sematary',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Skeleton Crew',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'The Eyes of the Dragon',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Misery',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'The Tommyknockers',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'The Dark Half',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'The Waste Lands: The Dark Tower Book III',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Needful Things: The Last Castle Rock Story',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Insomnia',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'The Girl Who Loved Tom Gordon',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'On Writing:  a Memoir of the Craft',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Dreamcatcher',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'From a Buick 8',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'The Dark Tower VI ',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Cell',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Finders Keepers',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'End of Watch',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'The Outsider',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Elevation',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'The Institute',
+//     //   "photoUrl":'',
+//     // },
+//     // {
+//     //   "photoTitle":'Christine',
+//     //   "photoUrl":''
+//     // }
+  ]
+
+
+ export default images;
