a22007156126515defbe59759d75f300a0a48cf3
bhohnco <bryanhohn13@gmail.com>
2021-06-29 09:09:45 -0600

    Add random book function on load

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -1,16 +1,27 @@
 
 const utils = {
 
-  removeDuplicates(objectArray) {
-    return objectArray.reduce((finalOutput, currentObj) => {
-      const findTitle = finalOutput.find(item => item.title === currentObj.title);
-      if (!findTitle) {
-        // finalOutput.key = toString().split('/')[2];
-        return finalOutput.concat([currentObj]).sort()
-      }
-      return finalOutput
-    }, [])
-  }
+  // removeDuplicates(objectArray) {
+  //   return objectArray.reduce((finalOutput, currentObj) => {
+  //     const findTitle = finalOutput.find(item => item.title === currentObj.title);
+  //     if (!findTitle) {
+  //       // finalOutput.key = toString().split('/')[2];
+  //       return finalOutput.concat([currentObj]).sort()
+  //     }
+  //     return finalOutput
+  //   }, [])
+  // }
 
+   randomArrayShuffle(array) {
+     let currentIndex = array.length, temporaryValue, randomIndex;
+     while (0 !== currentIndex) {
+       randomIndex = Math.floor(Math.random() * currentIndex);
+       currentIndex -= 1;
+       temporaryValue = array[currentIndex];
+       array[currentIndex] = array[randomIndex];
+       array[randomIndex] = temporaryValue;
+     }
+     return array;
+   }
   }
   export default utils;
